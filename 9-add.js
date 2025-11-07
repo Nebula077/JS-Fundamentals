7e43954aa1e1334c4bf474327931f52c764b6556
Allan <nabuleallan1@gmail.com>
2025-11-07 11:34:21 +0300

    added add.js

--- 9-add.js
+++ 9-add.js
@@ -1,5 +1,16 @@
-function add(a, b) {
-    answer = a + b;
-    console.log(answer);
+const args = process.argv.slice(2);
+
+// Expect exactly two arguments. Convert them to numbers and print the sum.
+if (args.length !== 2) {
+    console.log('NaN');
+} else {
+    const a = Number(args[0]);
+    const b = Number(args[1]);
+
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+        console.log('NaN');
+    } else {
+        console.log(a + b);
+    }
 }
 
