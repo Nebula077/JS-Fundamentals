70a1b50f81f5ef4d171200b009710335787f710a
Allan <nabuleallan1@gmail.com>
2025-11-07 10:58:39 +0300

    added to-integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
 if (!isNaN(args[0])) {
-    console.log("My Number: " + parseInt(args[0]));
+    console.log("My number: " + parseInt(args[0]));
 } else if (isNaN(args[0])) {
     args[0] = parseInt(args[0]);
-    console.log("My Number: " + parseInt(args[0]));
+    console.log("My number: " + parseInt(args[0]));
 } else {
-    console.log("Not a Number");
+    console.log("Not a number");
 }
